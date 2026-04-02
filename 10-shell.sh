c3b25453fd08f3eb45eac8a81fa508df16ae390f
sreekanth <sreekanth13324@gmail.com>
2026-04-02 09:23:30 +0530

    ten shell

---
+++ 10-shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+number=$1
+if [ $(number) -lt 10]; then
+    echo "the given number is less than 10"
+elif [ $(number) -eq 10 ]; then
+    echo "given number is equal to 10"
+else
+    echo "the given number is greater than 10"
+fi
